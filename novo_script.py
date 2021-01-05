46a66bf8b72c8b999357f4c5bcef3466b57e67fd
Nina-pinheiro <ninapinheiro@usp.br>
2021-01-05 16:40:39 -0200

    Atualização do script

--- novo_script.py
+++ novo_script.py
@@ -2,7 +2,7 @@
 # Importar bibliotecas
 import random 
 import argparse 
-
+from Bio import SeqIO
 # Código para probabilidades de mutação
 
 
@@ -36,30 +36,33 @@ def substituicao(seq_dna,prob_mutar,prob_gc):
     return "".join(lista_final)
   
 
+
+
+
 if  __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description = "Um programa que calcula as probabilidades de mutações")
-    parser.add_argument('--S','--sequencia', dest = "seq_dna",required = True, help = "Digite a Sequência", type = str)
-    parser.add_argument('--P','--probabilidade', dest = "prob_mutar",required = True, help = "Digite a probabilidade de ocorrência de mutação", type = float)
-    parser.add_argument('--G', '--prob', dest = "prob_gc", required = True, help = "Digite a probabilidade de ocorrência de GC", type = float)  
+    parser.add_argument('-F','--fastaFile', dest = "fasta_file",required = True, help = "Digite o nome do arquivo com as sequências em formato fasta", type = str)
+    parser.add_argument('-P','--probabilidade', dest = "prob_mutar",required = True, help = "Digite a probabilidade de ocorrência de mutação", type = float)
+    parser.add_argument('-G', '--prob', dest = "prob_gc", required = True, help = "Digite a probabilidade de ocorrência de GC", type = float)
     args = parser.parse_args()
-    seq_dna = args.seq_dna
+
     prob_mutar = args.prob_mutar
     prob_gc = args.prob_gc
-    seq_dna_duplicada = seq_dna + seq_dna
-    verificacao = substituicao(seq_dna,prob_mutar,prob_gc)
-    if len(verificacao) == len(seq_dna):
-        print("Está correto")
-    else:
-        print("Não está correto")
-        
-    
-    print(verificacao)
-    
-        
+
+    with open(args.fasta_file, 'rU') as fastahandle:
+      for seq_record in SeqIO.parse(fastahandle, 'fasta'):
+       print(seq_record.id)
+       verificacao = substituicao(seq_record.seq,prob_mutar,prob_gc)
+       if len(verificacao) == len(seq_record.seq):
+         print("Está correto")
+       else:
+         print("Não está correto")
 
 
+       print(verificacao)
     
+        
 
 
 
