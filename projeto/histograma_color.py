942a7be32e5a67ab99ad54056462d62556e72308
Nina-pinheiro <ninapinheiro@usp.br>
2021-05-15 23:57:29 -0300

    Atualizando

--- projeto/histograma_color.py
+++ projeto/histograma_color.py
@@ -34,13 +34,22 @@ if  __name__ == "__main__":
         for record in SeqIO.parse(fastqhandle, "fastq"):
             sequences[record.id] = GC(record.seq)
             gchandle.write(f'{record.id}\toriginal\t{sequences[record.id]}\n')
-    with open(args.fastq_file2,'r') as fastqhandle2, open(args.gc_file, 'w') as gchandle2:
-        for record in SeqIo.parse(fastqhandle2, "fastq2"):
-            sequences_mutado[record.id] = GC(record.seq)
-            gchandle2.write(f'{record.id}\tmutado\t{sequences_mutado[record.id]}\n')
-         
-    #fig = plt.hist(sequences.values(), bins = np.ceil(1+3.3*np.log(len(sequences))).astype(int))
-    fig = plt.hist([sequences.values(),sequences_mutado.values()],bins = 200,label = ['original','mutado'])
+    with open(args.fastq_file2,'r') as fastqhandle2, open(args.gc_file, 'a') as gchandle2:
+        for record in SeqIO.parse(fastqhandle2, "fastq"):
+             sequences_mutado[record.id] = GC(record.seq)
+             gchandle2.write(f'{record.id}\tmutado\t{sequences_mutado[record.id]}\n')
+
+    figure, (ax1,ax2) = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
+    ax1.hist(sequences.values(), bins = 1000,label= 'original',color='green',)
+    ax1.hist(sequences_mutado.values(), bins = 1000,label= 'mutado', color='red')
+    ax1.set_title("com 1000")
+    ax2.hist(sequences.values(), bins = 200,label= 'original',color='green',)
+    ax2.hist(sequences_mutado.values(), bins = 200,label= 'mutado', color='red')
+    ax2.set_title("com 200")
+    
+    #fig1 = plt.hist(sequences.values(), bins = np.ceil(1+3.3*np.log(len(sequences))).astype(int))
+    #fig2 = plt.hist([sequences.values(),sequences_mutado.values()],bins = 200,label = ['original','mutado'])
+    
     plt.savefig(args.output_file+'_colors.png', dpi=200, format='png')      
 
        
