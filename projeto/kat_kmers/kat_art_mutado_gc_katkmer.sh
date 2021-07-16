24d1ccf1d018da6638d894a97305b165ffef4af9
Nina-pinheiro <ninapinheiro@usp.br>
2021-07-15 23:34:05 -0300

    Adicionando script gc do genoma mutado para o art na pasta kat_kmers

---
+++ projeto/kat_kmers/kat_art_mutado_gc_katkmer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#$ -q all.q
+#$ -cwd
+#$ -t 1-10
+
+module load miniconda3
+export PATH="/Storage/data1/felipe.peres/src/KAT/bin/:$PATH"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/Storage/data1/felipe.peres/src/libtool-2.4.6/bin/:/Storage/data1/felipe.peres/src/KAT/deps/boost/build/lib/:/Storage/progs/miniconda3/lib/
+
+ 
+
+INFILE1=`ls -1 /Storage/data1/nina.pinheiro/iniciacao/HCKmer/projeto/saidas/saidas_reads/out_read_art_mutado_${SGE_TASK_ID}1.fq`
+INFILE2=${INFILE1/1.fq/2.fq}
+ 
+
+kat gcp -m 37 -o /Storage/data1/nina.pinheiro/iniciacao/HCKmer/projeto/kat_kmers/saidas_gc/kat_art_mutado_gc_37_${SGE_TASK_ID}  -t 1  "$INFILE1" "$INFILE2"
+
+
+
+
